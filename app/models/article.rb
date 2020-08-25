9c36a737a494a21ff322cc883c077d209363588d
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-25 21:27:07 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,22 +1,4 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-
-  attr_accessor :title, :content
-
-  @@article = []
-
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-    @@article << self
-  end
-
-  def self.all
-    @@article
-  end
-
-  def self.delete
-    @@article.clear
-  end
 end
